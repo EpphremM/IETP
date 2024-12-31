3f5e736bb25c1a34d700c63eda562a509bfe1d4e
EpphremM <you@example.com>
2024-12-30 21:50:01 -1000

    added

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-[#1a1b20] pt-3">
-        <div class="container flex mx-auto bg-blue-100 text-blue-900 mt-4 rounded-md p-4 justify-between items-center">
+        <div class="container  md:flex mx-auto bg-blue-100 text-blue-900 mt-4 rounded-md p-4 justify-between items-center">
   <!-- Brand Section -->
   <div>
     <h1 class="text-3xl sm:text-2xl font-bold text-blue-800">Modern</h1>
@@ -8,7 +8,7 @@
 
   <!-- Navigation Links -->
   <div>
-    <ul class="flex text-md space-x-6 justify-center sm:justify-start">
+    <ul class="flex text-md gap-2 md:space-x-6  justify-center sm:justify-start">
       <li>
         <nuxt-link
           to="/"
@@ -56,7 +56,7 @@
 
 
 
-        <section class="grid grid-cols-1 md:grid-cols-10 w-9/12 mx-auto justify-center place-items-center gap-8 mb-8 p-4">
+        <section id="home" class="grid grid-cols-1 md:grid-cols-10 w-9/12 mx-auto justify-center place-items-center gap-8 pt-8 mb-8 p-4">
   <div class="md:col-span-10 lg:col-span-5 flex flex-col justify-center">
     <h1 class="text-3xl md:text-4xl lg:text-5xl text-[#9ce1b6] text-left font-sans font-bold leading-tight">
       Revolutionizing Poultry Farming: Cleaner Air, Smarter Waste Management.
@@ -143,7 +143,7 @@
         <section id="gallery" class="mt-10 mx-auto">
   <div class="container mx-auto text-center">
     <h3 class="text-4xl text-left text-[#d3dad1] font-extrabold mb-6">Gallery</h3>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div class="grid grid-cols-1 p-5 md:grid-cols-2 lg:grid-cols-3 gap-8">
       <div class="group relative">
         <img
           src="/assets/images/new.jpg"
@@ -175,7 +175,6 @@
         </div>
       </div>
     </div>
-    <p class="mt-6 text-[#d3dad1]">Experience the transformation with our advanced chicken house designs.</p>
   </div>
 </section>
 
@@ -186,7 +185,7 @@
   <section id="team" class="p-8">
   <div class="container mx-auto">
     <h3 class="text-4xl text-[#d3dad1] font-extrabold mb-4">Team Members</h3>
-    <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="grid mx-auto md:grid-cols-2 lg:grid-cols-3 gap-6">
       <div
         v-for="member in members"
         :key="member.id"
@@ -202,7 +201,7 @@
   </div>
 </section>
 
-<section id="contact" class="w-9/12 mx-auto px-6 py-16 bg-inherit">
+<section id="contact" class="w-full md:w-9/12 p-5 mx-auto px-6 py-16 bg-inherit">
   <h3 class="text-4xl text-white text-left font-extrabold mb-6">Contact Us</h3>
   
   <div class="w-full mx-auto bg-[#1f2937] p-8 rounded-lg shadow-lg transform transition duration-300 hover:scale-105">
@@ -248,20 +247,52 @@
 
 <footer id="footer" class="bg-blue-100 text-blue-900 py-7 mt-8">
   <div class="container mx-auto flex flex-wrap justify-between items-center">
-    <!-- Left Section: Branding -->
-    <div class="mb-4 sm:mb-0">
-      <h1 class="text-3xl font-bold text-blue-900">Modern</h1>
-      <p class="text-lg text-blue-800">© 2024 Modern, All Rights Reserved</p>
-    </div>
 
-    <!-- Middle Section: Navigation Links -->
     <div class="mb-4 sm:mb-0 text-lg">
-      <ul class="flex space-x-8">
-        <li><nuxt-link to="/" class="hover:underline text-blue-800">Home</nuxt-link></li>
-        <li><nuxt-link to="/about" class="hover:underline text-blue-800">About</nuxt-link></li>
-        <li><nuxt-link to="/team" class="hover:underline text-blue-800">Team</nuxt-link></li>
-        <li><nuxt-link to="/contact" class="hover:underline text-blue-800">Contact Us</nuxt-link></li>
-      </ul>
+      <h1 class="text-3xl font-bold text-blue-900">Modern</h1>
+      <ul class="flex text-md gap-2 md:space-x-6  justify-center sm:justify-start">
+      <li>
+        <nuxt-link
+          to="#home"
+          class="text-blue-900 hover:text-blue-700 transition duration-300 ease-in-out"
+        >
+          Home
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link
+          to="#innovative"
+          class="text-blue-900 hover:text-blue-700 transition duration-300 ease-in-out"
+        >
+        Innovative
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link
+          to="#team"
+          class="text-blue-900 hover:text-blue-700 transition duration-300 ease-in-out"
+        >
+          Team
+        </nuxt-link>
+      </li>
+
+      <li>
+        <nuxt-link
+          to="#gallery"
+          class="text-blue-900 hover:text-blue-700 transition duration-300 ease-in-out"
+        >
+          Gallery
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link
+          to="/"
+          class="text-blue-900 hover:text-blue-700 transition duration-300 ease-in-out"
+        >
+          Contact
+        </nuxt-link>
+      </li>
+    </ul>
     </div>
 
     <!-- Right Section: Social Media Links -->
@@ -275,6 +306,9 @@
       <a href="https://linkedin.com" target="_blank" class="text-blue-600 hover:text-blue-800">
         <i class="fab fa-linkedin-in text-2xl"></i>
       </a>
+    </div>
+    <div class="mb-4 sm:mb-0">
+      <p class="text-lg text-blue-800">© 2024 Modern, All Rights Reserved</p>
     </div>
   </div>
 </footer>
@@ -298,7 +332,7 @@ export default {
                     departmentId: 'ETS0536/14',
                     role: 'Software Engineer',
                     phone: '+251938413473',
-                    otherContacts: 'epahaman48@gmail.com',
+                    otherContacts: 'ephrem.mandefro@aastustudent.edu.et',
                 },
                 {
                     id: 2,
@@ -306,7 +340,7 @@ export default {
                     departmentId: 'ETS1656/14',
                     role: 'Electrical Engineer',
                     phone: '+251946333013',
-                    otherContacts: 'yeabsraalemayehu592@gmail.com',
+                    otherContacts: 'yeabsra.alemayehu@aastustudent.edu.et',
                 },
                 {
                     id: 3,
@@ -314,7 +348,7 @@ export default {
                     departmentId: 'ETS0284/11',
                     role: 'Mechanical Engineer',
                     phone: '+251983407305',
-                    otherContacts: 'danielfentie2022@gmail.com',
+                    otherContacts: 'daniel.fentie@aastustudent.edu.et',
                 },
                 {
                     id: 3,
@@ -322,7 +356,7 @@ export default {
                     departmentId: 'ETS0611/14',
                     role: 'Environmental Engineer',
                     phone: '+251977746975',
-                    otherContacts: 'eyuelbefkaf@gmail.com',
+                    otherContacts: 'eyuel.befkadu@aastustudent.edu.et',
                 },
                 {
                     id: 3,
@@ -338,7 +372,7 @@ export default {
                     departmentId: 'ETS0125/14',
                     role: 'Electrical Engineer',
                     phone: '+251963039627',
-                    otherContacts: 'adonilol708@gmail.com',
+                    otherContacts: 'adoniyas.desalegn@aastustudent.edu.et',
                 },
                 {
                     id: 3,
@@ -346,7 +380,7 @@ export default {
                     departmentId: 'ETS1031/14',
                     role: 'Mining Engineer',
                     phone: '+251989923639',
-                    otherContacts: 'meklitzewde490@gmail.com',
+                    otherContacts: 'Meklit.zewude@aastustudent.edu.et',
                 },
                 {
                     id: 3,
@@ -354,15 +388,15 @@ export default {
                     departmentId: 'ETS1230/14',
                     role: 'Civil Engineer',
                     phone: '+251966000160',
-                    otherContacts: 'natnaelasaye5@gemail.com',
+                    otherContacts: 'natnael.asaye@aastustudent.edu.et',
                 },
                 {
                     id: 3,
                     name: 'Ewket Lemma',
-                    departmentId: '',
+                    departmentId: 'ETS0569/14',
                     role: 'Architect',
-                    phone: '555-123-4567',
-                    otherContacts: 'robert.brown@email.com',
+                    phone: '+251970947895',
+                    otherContacts: 'ewket.lemma@aastustudent.edu.et',
                 },
 
                 // Add more members as needed
